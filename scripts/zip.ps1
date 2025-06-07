32bdb94a3a968734e2ce8ecfe78c6440fc3e7c20
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2025-06-07 19:08:10 -0400

    golden one

--- scripts/zip.ps1
+++ scripts/zip.ps1
@@ -74,10 +74,10 @@ switch ($platform) {
         $vlcRoot = "/usr/lib/x86_64-linux-gnu/vlc"  # /usr/lib or /usr/lib/x86_64-linux-gnu depending on distro
         Copy-Item "/usr/lib/libvlc.so*" -Destination $binDir -Force
         Copy-Item "/usr/lib/libvlccore.so*" -Destination $binDir -Force
+        Copy-Item "/usr/lib/x86_64-linux-gnu/libvlc*" -Destination $binDir -Force
+        Copy-Item "/usr/lib/x86_64-linux-gnu/libvlccore*" -Destination $binDir -Force
         Copy-Item "$vlcRoot/libvlc*" -Destination $binDir -Force
         Copy-Item "$vlcRoot/libvlccore*" -Destination $binDir -Force
-        Copy-Item "$vlcRoot/libvlc.so*" -Destination $binDir -Force
-        Copy-Item "$vlcRoot/libvlccore.so*" -Destination $binDir -Force
         Copy-Item "$vlcRoot/plugins/*" -Destination $pluginsDir -Force
 
         #Copy-Item "/usr/lib/vlc/plugins/*" -Destination $pluginsDir -Recurse -Force
